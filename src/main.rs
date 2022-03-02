ecc8b6203ef99c223aca41aa3ee2de610946c2e7
shunak <shuna966@gmail.com>
2022-03-02 12:18:28 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html{
+    html! {
+        <h1>{ "Hello World"}</h1>
+    }
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
